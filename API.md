867960f6d115d2f28fee068f8af8ed5c2a76f6c3
charles.antigua@billet.do <charles.antigua@billet.do>
2025-04-29 23:46:55 -0400

    initial commit

---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="cdk-demo.Hello"></a>
+
+#### Initializers <a name="Initializers" id="cdk-demo.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from 'cdk-demo'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#cdk-demo.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="cdk-demo.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
---
+++ src/LambdaConstruct.ts
@@ -0,0 +1,28 @@
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+
+
+export interface CustomLambdaProps {
+  readonly name: string;
+  readonly runtime: lambda.Runtime;
+  readonly handler: string; // example 'index.handle'
+  readonly codeDir: string; // example '/lambda/function
+  readonly version: number;
+}
+
+export class CustomLambda extends Construct {
+  readonly LambdaName: string;
+  constructor(parent: Construct, name: string, props: CustomLambdaProps) {
+    super(parent, name);
+
+
+    const customlambda = new lambda.Function(this, 'Function', {
+      runtime: props.runtime,
+      handler: props.handler,
+      code: lambda.Code.fromAsset(props.codeDir),
+    });
+
+    this.LambdaName = customlambda.functionName;
+  }
+
+}
